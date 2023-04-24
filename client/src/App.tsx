c5ab9214db96ee24d02c44440d491caee22dfb3b
Daria Malone <thefashionphysician@gmail.com>
2023-04-23 23:45:20 -0700

    started account page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Home from './Components/Home'
 import Register from './Components/Register'
 import axios from 'axios'
 import { UserContextProvider } from './UserContext'
+import Account from './Components/Account'
 
 
 axios.defaults.baseURL = 'http://localhost:4000'
@@ -20,13 +21,13 @@ function App() {
       <Routes>
       <Route path='/' element={<Layout />}>
         {/* public routes */}
-        <Route index element={<Public />}></Route>
-        <Route path='/auth/login' element={<Login />}></Route>
-        <Route path='/auth/register' element={<Register />}></Route>
+        <Route index element={<Public />}/>
+        <Route path='/auth/login' element={<Login />}/>
+        <Route path='/auth/register' element={<Register />}/>
 
         <Route path='dash' element={<DashLayout />}>
-          <Route index element={<Home />}></Route>
-
+          <Route index element={<Home />}/>
+          <Route path='/account' element={<Account />}/>
 
         </Route>{/* End Dash */}
 
--- client/src/Components/Account.tsx
+++ client/src/Components/Account.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../UserContext'
+import { Navigate } from 'react-router-dom'
 
 const Account = () => {
+    const {user} = useContext(UserContext)
+
+    if (!user) {
+        return <Navigate to={'/login'}/>
+    }
+
   return (
     <div>Account</div>
   )
