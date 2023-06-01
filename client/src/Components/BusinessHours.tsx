7e1327b9c21fd7b436b634dfd19b6a105eed9575
Daria Malone <thefashionphysician@gmail.com>
2023-05-31 23:18:26 -0700

    updated

--- client/src/Components/BusinessHours.tsx
+++ client/src/Components/BusinessHours.tsx
@@ -8,235 +8,159 @@ type BusisnessProps =  {
 export default function BusinessHours(props: BusisnessProps) {
   const {hours, setHrs} = props
   const [checked, setChecked] = useState(false)
-  const [openField, setOpenField] = useState("")
-  const [closeField, setCloseField] = useState("")
+ 
 
   function handleTime(e:any) {
-    const {name, value} = e.target
+    const {type, name, value} = e.target
     if(checked) {
-      setHrs({...hours, [name]: value}) 
-    } else{
-      setHrs({...hours, [name]: '0'})
-    }
+      console.log(value)}
+      // setHrs({...hours, [name]: value}) 
+    // } else{
+    //   setHrs({...hours, [name]: '0'})
+    // }
+
   }
 
   return (
     <>
-      <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
+      <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" onChange={e => handleTime(e)}>
         <label><input 
           type='checkbox' 
-          value={1} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Sunday </label>
+          value={'Sunday'} 
+       /> Sunday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              />
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
-              name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              name='closeHrs'  
+              />
         </div>
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={4} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Monday </label>
+          /> Monday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>  
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={5} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Tuesday </label>
+          /> Tuesday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>     
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={6} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Wednesday </label>
+          /> Wednesday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>        
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={7} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Thursday </label>
+          /> Thursday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>     
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={1} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Friday </label>
+          /> Friday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>   
       </div>
       <div className="border grid sm:grid-cols-3 p-2 rounded-2xl items-center cursor-pointer my-2" >
       <label><input 
           type='checkbox' 
           value={1} 
-          onChange={() => {
-            if(checked){
-              setOpenField('')
-              setCloseField('')
-            }
-            setChecked(!checked)
-          }
-       }/> Saturday </label>
+          /> Saturday </label>
         <div>
             <h3>Open</h3>
             <input 
               type='time' 
               name='openHrs' 
-              disabled={!checked}
-              value={openField}
-              onChange= {e => [setOpenField(e.target.value), handleTime(e)]}/>
+              onChange= {e => handleTime(e)}/>
         </div>
         <div>
             <h3>Close</h3>
             <input 
               type='time' 
               name='closeHrs' 
-              disabled={!checked}
-              value={closeField} 
-              onChange={e => [setCloseField(e.target.value), handleTime(e)]}/>
+              onChange={e => handleTime(e)}/>
         </div>  
       </div>
     </>
